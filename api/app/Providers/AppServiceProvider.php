68c6be6f0207cdb7eaaa5a2c809fe09117a35187
Thaiza Medeiros <thaizamedeirossw@gmail.com>
2020-06-07 20:29:11 -0300

    Configurando o comprimento da string das migrations manualmente.

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Se a versão do MySQL for anterior a versão 5.7.7 ou MariaDB anterior à versão 10.2.2
+        // (configure o comprimento da string manualmente)
+        Schema::defaultStringLength(191);
     }
 }
